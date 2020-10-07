5ee26720925917144cc13272aeb4d96a7d3d31eb
Nguyen Phuc Chuong <chuongnp@viettel.com.vn>
2020-10-07 13:34:45 +0700

    add hello api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+	@GetMapping("/")
+	public String hello(){
+		return "Hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
